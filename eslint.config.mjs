1d9314e3e0148b15e8b4f72c8fd15fe20d802457
YCChen <victor.yong.jhe.chen@gmail.com>
2024-06-09 13:49:38 +0800

    resolve elsint cannot know react extend

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,9 +4,9 @@ import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import { fixupConfigRules } from "@eslint/compat";
 
-
 export default [
   {languageOptions: { globals: globals.browser }},
+  "react-app",
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...fixupConfigRules(pluginReactConfig),
