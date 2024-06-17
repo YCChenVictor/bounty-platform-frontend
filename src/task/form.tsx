e25ac88e432593bf4d5c788c6fa97e54f882d6af
Victor Chen <victor.chen@hoc-trade.com>
2024-06-17 23:18:38 +0800

    Refactor TaskForm component to remove unused code and simplify onTaskCreate function signature

--- src/task/form.tsx
+++ src/task/form.tsx
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
-import { TaskModal } from './modal';
 
 interface TaskFormProps {
-  onTaskCreate: (name: string, repo: string) => void;
+  onTaskCreate: (name: string) => void;
 }
 
 export const TaskForm: React.FC<TaskFormProps> = ({ onTaskCreate }) => {
   const [newTaskName, setNewTaskName] = useState('');
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleNewTaskNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNewTaskName(event.target.value);
@@ -15,13 +13,7 @@ export const TaskForm: React.FC<TaskFormProps> = ({ onTaskCreate }) => {
 
   const handleNewTaskSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    setIsModalOpen(true);
-  };
-
-  const handleRepoSubmit = (repo: string) => {
-    onTaskCreate(newTaskName, repo);
-    setNewTaskName('');
-    setIsModalOpen(false);
+    onTaskCreate(newTaskName);
   };
 
   return (
@@ -30,7 +22,6 @@ export const TaskForm: React.FC<TaskFormProps> = ({ onTaskCreate }) => {
         <input type="text" value={newTaskName} onChange={handleNewTaskNameChange} />
         <button type="submit">Create Task</button>
       </form>
-      <TaskModal isOpen={isModalOpen} onRepoSubmit={handleRepoSubmit} />
     </div>
   );
 };
