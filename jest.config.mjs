ed19f1722c5d9f4cc6180a408667d59f170ff01c
Victor Chen <victor.chen@hoc-trade.com>
2024-06-19 18:18:10 +0800

    chore: Update Jest configuration to collect coverage from src/**/*.tsx

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,5 +1,6 @@
-module.exports = {
+export default {
   collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.tsx'],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   preset: 'ts-jest',
