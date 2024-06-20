48c07b05edd489ab9fc9f5246e07b4bb9a32925e
Victor Chen <victor.chen@hoc-trade.com>
2024-06-20 21:24:01 +0800

    feat: Add checkbox functionality to TaskList component

--- src/task/list.tsx
+++ src/task/list.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface TaskListProps {
   tasks: string[];
 }
 
 const TaskList: React.FC<TaskListProps> = ({ tasks }) => {
+  const [completedTasks, setCompletedTasks] = useState<number[]>([]);
+
+  const markAsCompleted = (index: number) => {
+    if (completedTasks.includes(index)) {
+      setCompletedTasks(completedTasks.filter(item => item !== index));
+    } else {
+      setCompletedTasks([...completedTasks, index]);
+    }
+  };
+
   return (
     <ul>
       {tasks.map((task, index) => (
-        <li key={index}>{task}</li>
+        <li key={index}>
+          <input
+            type="checkbox"
+            checked={completedTasks.includes(index)}
+            onChange={() => markAsCompleted(index)}
+          />
+          <span style={{ textDecoration: completedTasks.includes(index) ? 'line-through' : 'none' }}>
+            {task}
+          </span>
+        </li>
       ))}
     </ul>
   );
--- tests/index.test.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import TestRenderer from 'react-test-renderer';
-
-it('renders without crashing', () => {
-  const testRenderer: TestRenderer.ReactTestRenderer = TestRenderer.create(<h1>Hello World</h1>);
-  expect(testRenderer.toJSON()).toMatchSnapshot();
-});
