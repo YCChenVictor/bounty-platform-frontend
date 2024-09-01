01058252141341251b61860b1a2f0abc602e7c09
Victor Chen <victor.chen@hoc-trade.com>
2024-09-01 19:08:40 +0800

    test: add test for App.tsx

--- tests/task/list.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/react";
-import React from "react";
-import TaskList from "../../src/task/list";
-
-test("renders without crashing", () => {
-  const tasks = ["Task 1", "Task 2", "Task 3"];
-  render(<TaskList tasks={tasks} />);
-});
